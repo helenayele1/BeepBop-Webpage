66728183ccc4edd0386fc3d59af4344a7b70a453
Padma Patil <padma@email.com>
2023-11-25 16:32:03 -0800

    add beep function to js file

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//BU Logic
+
+
+function beepBoop(number) {
+    const result = [];
+    for (let num = 0; num <= number; num++) {
+      let substitution = num.toString();
+    }
+ if (num.toString().includes('3')) {
+        substitution = "Won't you be my neighbor?";
+      }
+
+}
