bb7d66758ab9e2e84f5d1bf693095d003a1e4328
Padma Patil <padma@email.com>
2023-11-27 17:54:35 -0800

    update html and js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,29 @@
+//UI logic
+window.onload = function() {
+    const form = document.getElementById('input-form');
+    const resultContainer = document.getElementById('result-container');
+   
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const userNumber = parseInt(document.getElementById('number').value);
+        const resultArray = beepBoop(userNumber);
+        displayResult(resultArray);
+      });
+    
+      function displayResult(resultArray) {
+        resultContainer.innerHTML = '';
+        const ul = document.createElement('ul');
+        resultArray.forEach(function(item) {
+          const li = document.createElement('li');
+          li.textContent = item;
+          ul.appendChild(li);
+        });
+        resultContainer.appendChild(ul);
+      }
+    };
+
+
 //BU Logic
-
-
 function beepBoop(number) {
     const result = [];
     for (let num = 0; num <= number; num++) {
